68e8ac369600b287bdd9d46878b42dc96ba7548c
ameeqcode <waqas@ameeqcode.com>
2024-06-28 17:15:59 +0500

    Update api.rs
    Updated function names

--- src/api/api.rs
+++ src/api/api.rs
@@ -10,9 +10,9 @@ pub struct POSOuterArray {
 
 
 #[post("/pos-items")]
-pub async fn pos_items_import(web::Json(data): web::Json<POSOuterArray>, db: web::Data<Database>) -> HttpResponse {
+pub async fn import_pos_items_from_json(web::Json(data): web::Json<POSOuterArray>, db: web::Data<Database>) -> HttpResponse {
     if let Some(inner_objects) = data.outer_array {
-            let _ = db.pos_items_imports(inner_objects);
+            let _ = db.import_pos_items(inner_objects);
     }
     HttpResponse::Ok().json("POS items import successful.")
 }
@@ -21,6 +21,6 @@ pub async fn pos_items_import(web::Json(data): web::Json<POSOuterArray>, db: web
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/api")
-            .service(pos_items_import)
+            .service(import_pos_items_from_json)
     );
 }
